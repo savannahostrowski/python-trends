a769b3cc1e6387efd1ac178c41ff4462d9757e00
Savannah Ostrowski <savannahostrowski@gmail.com>
2021-03-02 16:01:44 -0800

    Initial commit

---
+++ json2csv.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+"""
+Tabulate the output JSON files from pypi-trends.py into a CSV file, pypi-trends.csv
+"""
+import argparse
+import csv
+import glob
+import hashlib
+import json
+import os
+import re
+import traceback
+from collections import defaultdict
+from pprint import pprint
+
+from natsort import natsorted  # pip install natsort
+from packaging.version import parse  # pip install packaging
+from termcolor import cprint  # pip install termcolor
+
+
+# https://stackoverflow.com/a/5734564/724176
+def month_year_iter(start_month, start_year, end_month, end_year):
+    ym_start = 12 * start_year + start_month - 1
+    ym_end = 12 * end_year + end_month - 1
+    for ym in range(ym_start, ym_end):
+        y, m = divmod(ym, 12)
+        yield y, m + 1
+
+
+def inspec_to_name(inspec):
+    """
+    data/X* -> X
+    data/X*.json -> X
+    data/201*.json -> None
+    """
+    name = os.path.basename(inspec).split("*")[0]
+    if name.isdigit():
+        return None
+    return name
+
+
+def dopplr(name):
+    """
+    Take the MD5 digest of a name,
+    convert it to hex and take the
+    first 6 characters as an RGB value.
+    """
+    # Tweak "2.8" because it's too close in colour to "3.5"
+    if name == "2.8":
+        name = "python 2.8"
+
+    return "#" + hashlib.sha224(name.encode()).hexdigest()[:6]
+
+
+# https://python-graph-gallery.com/255-percentage-stacked-area-chart/
+def make_chart(data, index, project_name, no_show, quiet):
+
+    grand_total_downloads = 0
+    for version in data:
+        grand_total_downloads += sum(dls for dls in data[version])
+
+    import matplotlib.pyplot as plt  # pip install matplotlib
+    import numpy as np  # pip install numpy
+    import pandas as pd  # pip install pandas
+    from matplotlib.ticker import FuncFormatter
+
+    labels = data.keys()
+
+    data = pd.DataFrame(data, index=index)
+
+    # We need to transform the data from raw data to percentage (fraction)
+    data_perc = data.divide(data.sum(axis=1), axis=0)
+
+    # Use a hash function to always use the same colour for each version number,
+    # regardless of which versions are present in this data
+    colors = [dopplr(s) for s in labels]
+    if not quiet:
+        print(colors)
+
+    # Make the plot
+    plt.stackplot(index, data_perc.T, labels=labels, colors=colors)
+
+    ax = plt.gca()
+
+    # Convert Y axis from 0..1 to 0%..100%
+    ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: f"{y:.0%}"))
+
+    # Major ticks every 10% or 0.1
+    major_ticks = np.arange(0, 1.2, 0.1)
+    ax.set_yticks(major_ticks)
+
+    # Set X labels to 2016-01, 02, ... 12, 2017-01, 02, ...
+    x_labels = []
+    last_year = None
+    for year_month in index[:-1]:
+        year, month = year_month.split("-")
+        if year == last_year:
+            x_labels.append(month)
+            # if month in ["04", "07", "10"]:
+            # if month in ["03", "05", "07", "09", "11"]:
+            #     x_labels.append(month)
+            # else:
+            #     x_labels.append("")
+        else:
+            x_labels.append(year_month)
+            last_year = year
+    x_labels.append(index[-1])  # No change for last one
+    ax.set_xticklabels(x_labels, fontsize=7)
+
+    plt.xticks(rotation=90)
+
+    # Pad margins so that markers don't get clipped by the axes
+    # plt.margins(0.2)
+
+    # Tweak spacing to prevent clipping of tick-labels
+    plt.subplots_adjust(bottom=0.2)
+
+    plt.grid()
+
+    plt.margins(0, 0)
+
+    # Shrink current axis by 20% so legend is outside chart
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.9, box.height])
+
+    # To reverse the legend order
+    handles, labels = ax.get_legend_handles_labels()
+
+    # Put a legend to the right of the current axis
+    ax.legend(
+        handles[::-1],
+        labels[::-1],
+        loc="center left",
+        bbox_to_anchor=(1, 0.5),
+        fontsize=8,
+    )
+
+    if project_name:
+        s = "" if project_name.endswith("s") else "s"
+        title = (
+            f"{project_name}’{s} pip installs from PyPI over time, by Python version"
+        )
+    else:
+        title = "pip installs from PyPI over time, by Python version"
+        project_name = "all"
+    plt.suptitle(title)
+    plt.title(f"{grand_total_downloads:,d} total downloads", fontsize=10)
+
+    outfile = f"images/{project_name}.png"
+    cprint(outfile, "green")
+    plt.savefig(outfile, dpi=96 * 2.5)
+    if not no_show:
+        plt.show()
+
+
+def remove_from_list(items, the_list):
+    for item in items:
+        if item in the_list:
+            the_list.remove(item)
+    return the_list
+
+
+def load_data_from_json(inspec, quiet=True):
+    all_data = []
+    all_versions = set()
+
+    files = glob.glob(inspec)
+    files = sorted(files)
+    # Skip data for top_repos.py
+    files = remove_from_list(
+        ["data/top-pypi-packages.json", "data/top-repos.json"], files
+    )
+
+    for f in files:
+        # Get the yyyy-dd from the filename
+        month_name = "".join(re.findall(r"\d{4}-\d{2}", f))
+        if not quiet:
+            print(f, month_name)
+        with open(f) as json_data:
+            try:
+                d = json.load(json_data)
+            except json.decoder.JSONDecodeError as e:
+                cprint(traceback.format_exc(), "red")
+                cprint(f"Skipping {f}: {e}", "yellow")
+                continue
+            # pprint(d)
+            month_data = defaultdict(int)
+            month_data["yyyy-mm"] = month_name
+            try:
+                # pypinfo
+                rows = d["rows"]
+                version_index = "python_version"
+                downloads_index = "download_count"
+            except KeyError:
+                # pypistats
+                rows = d["data"]
+                version_index = "category"
+                downloads_index = "downloads"
+            for row in rows:
+                version = row[version_index]
+                base_version = parse(version).base_version  # strips a4, b2, rc1
+
+                # Skip unknown versions
+                if version in ["None", "null", "Sure.0"]:
+                    continue
+
+                # month_data[row["python_version"]] = float(row["percent"]) * 100
+                month_data[base_version] += row[downloads_index]
+                all_versions.add(base_version)
+        all_data.append(month_data)
+
+    if not quiet:
+        pprint(all_versions)
+    all_versions = natsorted(all_versions)
+
+    return all_data, all_versions
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("-i", "--inspec", default="data/*.json", help="Input file spec")
+    parser.add_argument(
+        "-p", "--project", help='Project; shortcut for -i "data/project*.json"'
+    )
+    parser.add_argument("-c", "--chart", action="store_true", help="Create a chart")
+    parser.add_argument(
+        "-ns", "--no-show", action="store_true", help="Don't show the chart"
+    )
+    parser.add_argument("-q", "--quiet", action="store_true", help="Show less output")
+    args = parser.parse_args()
+
+    if args.project:
+        args.inspec = f"data/{args.project}*.json"
+
+    all_data, all_versions = load_data_from_json(args.inspec, args.quiet)
+
+    f = csv.writer(open(os.path.join("data", "pypi-trends.csv"), "w+"))
+    # f.writerow(["", "Python version"])
+    f.writerow(["Month"] + all_versions)
+    rows = []
+    for x in all_data:
+        row = [x["yyyy-mm"]]
+        for version in all_versions:
+            row.append(x.get(version, 0))
+        f.writerow(row)
+        rows.append(row)
+
+    if args.chart:
+        data = {}
+        index = []
+
+        # Initialise dict
+        for version in all_versions:
+            # print(version)
+            data[version] = []
+
+        for month_data in all_data:
+            index.append(month_data["yyyy-mm"])
+            for version in all_versions:
+                downloads = month_data.get(version, 0)
+                data[version].append(downloads)
+
+        make_chart(data, index, inspec_to_name(args.inspec), args.no_show, args.quiet)
+
+
+if __name__ == "__main__":
+    main()
+
+# End of file
---
+++ pypi-trends.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+"""
+For a given project, or all projects:
+* Fetch pip installs for each Python in a given month of a year
+* Save in a JSON file for each month of a year
+
+Requires pypinfo to be installed and configured
+* https://github.com/ofek/pypinfo
+
+Alternatively, requires pypistats to be installed
+(note: only has 6 months of stats and output JSON is a different format)
+* pip install -U pypistats
+
+Notes:
+    "Data ingestion into the BigQuery data set was spotty prior to June 2016
+    (but it shouldn't be biased, so these percentages are likely to be accurate),
+    but you can see a significant uptick in Python 3 based downloads over 2016.
+    If these trends continue..."
+https://langui.sh/2016/12/09/data-driven-decisions/
+"""
+import argparse
+import os
+import subprocess
+import sys
+from datetime import date, datetime
+
+from dateutil.relativedelta import relativedelta  # pip install python-dateutil
+from termcolor import colored, cprint  # pip install termcolor
+
+now = date.today()
+
+
+# https://stackoverflow.com/a/5734564/724176
+def month_year_iter(start_month, start_year, end_month, end_year, reverse=False):
+    ym_start = 12 * start_year + start_month - 1
+    ym_end = 12 * end_year + end_month - 1
+    ym_range = range(ym_start, ym_end)
+    if reverse:
+        ym_range = reversed(ym_range)
+    for ym in ym_range:
+        y, m = divmod(ym, 12)
+        yield y, m + 1
+
+
+def spread_order(years_months):
+    """
+    Sort so months are in this order:
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    =>
+    [1, 7, 4, 10, 3, 6, 9, 12, 2, 5, 8, 11]
+
+    :param years_months: list of (year, month) tuples
+    :return: re-sorted list of (year, month) tuples
+    """
+    order = [1, 7, 4, 10, 3, 6, 9, 12, 2, 5, 8, 11]
+    output = []
+    for o in order:
+        for year, month in years_months:
+            if month == o:
+                # print(year, month)
+                output.append((year, month))
+    return output
+
+
+def six_months_ago():
+    """For --pypistats, the earliest start date is six months ago"""
+    first = now - relativedelta(months=6)
+    return f"{first.year}-{first.month:02}"
+
+
+def default_end_date():
+    """For the default end date"""
+    last = now - relativedelta(months=1)
+    return f"{last.year}-{last.month:02}"
+
+
+def yyyy_mm_to_ints(yyyy_mm):
+    """Return yyyy-mm as two integers"""
+    try:
+        the_date = datetime.strptime(yyyy_mm, "%Y-%m")
+    except ValueError:
+        raise ValueError("Dates must be YYYY-MM")
+
+    return the_date.year, the_date.month
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-p", "--package", default="''", help="Show data for this package"
+    )
+    parser.add_argument(
+        "-f", "--from", dest="from_date", default="2016-01", help="Start YYYY-MM"
+    )
+    parser.add_argument(
+        "-t", "--to", dest="to_date", default=default_end_date(), help="End YYYY-MM"
+    )
+    parser.add_argument(
+        "-s", "--spread", action="store_true", help="Fetch data in spread order"
+    )
+    parser.add_argument(
+        "-n", "--dry-run", action="store_true", help="Don't execute pypinfo/pypistats"
+    )
+    parser.add_argument(
+        "--pypistats",
+        action="store_true",
+        help="Use pypistats instead of pypinfo. Note: only has "
+        "6 months of stats, and output JSON is a different "
+        "format.",
+    )
+    args = parser.parse_args()
+
+    if args.pypistats:
+        earliest_start_date = six_months_ago()
+        if args.from_date < earliest_start_date:
+            args.from_date = earliest_start_date
+            cprint(f"Set earliest start date for pypistats: {args.from_date}", "yellow")
+
+    from_year, from_month = yyyy_mm_to_ints(args.from_date)
+    to_year, to_month = yyyy_mm_to_ints(args.to_date)
+
+    print(
+        "Getting data from {} {} to {} {}\n".format(
+            from_month, from_year, to_month, to_year
+        )
+    )
+
+    years_months = list(
+        month_year_iter(from_month, from_year, to_month + 1, to_year, reverse=True)
+    )
+    if args.spread:
+        years_months = spread_order(years_months)
+
+    greens, yellows, reds = 0, 0, 0
+
+    for year, month in years_months:
+        if year == 2016 and month == 4:
+            cprint("  No data for 2016-04, skipping", "yellow")
+            continue
+        first = date(year, month, 1)
+        last = first + relativedelta(months=1) - relativedelta(days=1)
+        print(first, last)
+
+        if last >= now:
+            cprint("  End date should be in the past", "red")
+            reds += 1
+            sys.exit(1)
+
+        if args.package in ['""', "''"]:
+            prefix = ""
+        else:
+            prefix = args.package + "-"
+        outfile = f"{prefix}{year}-{month:02d}.json"
+        outfile = os.path.join("data", outfile)
+        if os.path.isfile(outfile):
+            cprint(f"  {outfile} exists, skipping", "yellow")
+            yellows += 1
+            continue
+
+        if args.pypistats:
+            cmd = (
+                f"pypistats python_minor {args.package} --json "
+                f"--start-date {first} --end-date {last} > {outfile}"
+            )
+        else:
+            cmd = (
+                f"pypinfo --start-date {first} --end-date {last} --percent --limit 100 "
+                f"--json {args.package} pyversion > {outfile}"
+            )
+
+        print(cmd)
+        executable = "pypistats" if args.pypistats else "pypinfo"
+        if args.dry_run:
+            print(f"  Dry run, not executing {executable}")
+        else:
+            # os.system(cmd)
+            exitcode, output = subprocess.getstatusoutput(cmd)
+            if exitcode == 0:
+                cprint(f"  {outfile}", "green")
+                greens += 1
+            else:
+                error = output.splitlines()[-1]
+                for line in output.splitlines():
+                    if "google.api_core.exceptions" in line:
+                        error = line
+                        break
+                cprint(error, "red")
+
+                reds += 1
+                if os.path.getsize(outfile) == 0:
+                    os.remove(outfile)
+
+    print(
+        colored(f"{greens}", "green"),
+        colored(f"{yellows}", "yellow"),
+        colored(f"{reds}", "red"),
+    )
+
+
+if __name__ == "__main__":
+    main()
+
+# End of file
